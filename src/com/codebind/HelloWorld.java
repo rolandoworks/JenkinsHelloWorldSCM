ba211df80b6f28c21b47ea6a0d9c7e3e910572de
rolandoworks <rolandoworks@gmail.com>
2022-06-11 16:04:37 -0400

    changes to be picked up by Jenkins POLL SCM

--- src/com/codebind/HelloWorld.java
+++ src/com/codebind/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("*****Hello World & Welcome to Jenkins Training*****");
-
+		System.out.println("Being pulled from a Jenkins instance to be built and run");
 	}
 	/***
 	 * Run Project from outside of Jenkins
