795fdded3886b08174939e91181c0dc805922a23
rolandoworks <rolandoworks@gmail.com>
2022-06-11 15:29:47 -0400

    adding Java Demo project

---
+++ src/com/codebind/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.codebind;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("*****Hello World & Welcome to Jenkins Training*****");
+
+	}
+	/***
+	 * Run Project from outside of Jenkins
+	 * JENKINS_URL/job/HelloWorld/build?token=TOKEN_NAME or /buildWithParameters?token=TOKEN_NAME
+	 * http://192.241.159.67:8080/jenkins/job/HelloWorld/build?token=testing123
+	 */
+
+		
+}
